b4ab18e96f7e918abc09bad8a560521563a2d291
Felipe Barras <felipeu_santos@hotmail.com>
2025-04-21 23:40:21 -0300

    fix: removendo o token cognito do upload.html

--- src/routes/frame.routes.js
+++ src/routes/frame.routes.js
@@ -6,7 +6,7 @@ const frameController = require('../controllers/frame.controller');
 router.post('/', authenticate, frameController.createFrame);
 router.get('/', authenticate, frameController.getFrameByUsername);
 router.get('/:frameId', authenticate, frameController.getFrameById);
-router.get('/:frameId/upload', authenticate, frameController.getUploadURL);
+router.get('/:frameId/upload', frameController.getUploadURL);
 router.patch('/:frameId/uploaded', authenticate, frameController.confirmUpload);
 router.delete('/', authenticate, frameController.deleteAllFrames);
 
